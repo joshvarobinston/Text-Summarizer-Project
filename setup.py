6c85da59bcf0a91e6c7782b6ef079f7b39fb98c6
joshvarobinston <joshestu08@gamil.com>
2026-01-03 14:22:42 +0530

    requirements are installed

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "joshvarobinston"
+SRC_REPO = "TextSummarizer"
+AUTHOR_EMAIL = "joshestu08@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Text Summarization Project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
